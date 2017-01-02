48e60ce5cbc2eee99bfa8da4c06664e948bc29da
Fabian Kästner <kruemel01@users.noreply.github.com>
2017-01-02 17:16:07 +0100

    Clean up main js

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-$(
-function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-  $(document).scroll(function() {
-	  console.log($(document).scrollTop());
-	  if ($(document).scrollTop() > 500) {
-		  $('.navEntry').css({
-			  backgroundPosition: '0 0',
-		  });
-	  } else {
-		  $('.navEntry').css({
-			  backgroundPosition: '0 -100%',
-		  });
-	  }
-  })
+function scrollHandler() {
+	if ($(document).scrollTop() > 500) {
+		$('.navEntry').css({
+			backgroundPosition: '0 0',
+		});
+	} else {
+		$('.navEntry').css({
+			backgroundPosition: '0 -100%',
+		});
+	}
 }
-);
+
+function linkHandler() {
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    	var target = $(this.hash);
+    	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    	if (target.length) {
+    		$('html, body').animate({
+    			scrollTop: target.offset().top
+        	}, 500);
+        	return false;
+      	}
+    }
+}
+
+
+$(function() {
+	$('a[href*="#"]:not([href="#"])').click(linkHandler);
+	$(document).scroll(scrollHandler);
+});
